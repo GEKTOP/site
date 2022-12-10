4eda45c329153a3a6782f0bcda443224e5929f8d
GEKTOP <119486733+GEKTOP@users.noreply.github.com>
2022-12-10 18:15:29 +0700

    добавил решение к задаче треугольника

---
+++ js/Задачи/треугольник.js
@@ -0,0 +1,11 @@
+let a = 145
+let b = 145
+let c = 139
+if (a <= b <= c) {
+    console.log("Равнобедрeнный")
+} else {
+    console.log("Равносторонний")
+}
+if (a != b != c) {
+    console.log("Разносторонний")
+}
