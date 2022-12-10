ae6e8f9cf36ce8d31be895f937e0e1e4ca0ab7d2
GEKTOP <119486733+GEKTOP@users.noreply.github.com>
2022-12-10 15:14:10 +0700

    домашка

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-    let user = "Дмитрий"
-    let a = 600
-    let b = 700
-    alert (" Hello, "+user+" Вот твое число "+(a+b))
+let a = 145
+let b = 145
+let c = 139
+if (a <= b <= c) {
+    console.log("Равнобедрeнный")
+} else {
+    console.log("Равносторонний")
+}
+if (a != b != c) {
+    console.log("Разносторонний")
+}
+let js = "JS прекрасен";
+for (js = 0; js < 10; js++) {
+    console.log("JS прекрасен", js);
+
+}
+let aaa = 'aaa'
+let bbbb = 'bbbb'
+let ttttt = 'ttttt'
+for (aaa = 0; aaa < 6; aaa++) {
+    console.log("aaa", aaa);
+}
+for (bbbb = 0; bbbb < 5; bbbb++) {
+    console.log("bbbb", bbbb);
+}
+for (ttttt = 0; ttttt < 9; ttttt++) {
+    console.log("ttttt", ttttt);
+}
+//до звезд не достал
